01ec30280212304fc28e5f9c963e06ca9cfc7249
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-10-02 10:25:43 +0700

    feat: enhance PublicChat component with WebSocket integration and chat history loading

--- src/app/chatroom/PublicChat.tsx
+++ src/app/chatroom/PublicChat.tsx
@@ -1,16 +1,50 @@
+import { useEffect, type Dispatch, type SetStateAction } from "react";
 import { MessageInput } from "../../components/MessageInput";
 import { MessageList } from "../../components/MessageList";
 import type { IChatMessage } from "../../types/ChatMessage";
+import type { WebSocketType } from ".";
 
 export default function PublicChat({
   messages,
   roomId,
   sendChat,
+  ws,
+  username,
+  setMessages,
 }: {
   messages: IChatMessage[];
   roomId: string;
   sendChat: (text: string) => void;
+  ws: WebSocketType;
+  username: string;
+  setMessages: Dispatch<SetStateAction<IChatMessage[]>>;
 }) {
+  useEffect(() => {
+    console.log(`Setting up chat room for ${roomId} with user ${username}`);
+
+    // Connect WebSocket
+    ws.connect();
+
+    // load chat history via rest
+    fetch(`http://localhost:8080/api/rooms/${roomId}/history`)
+      .then((res) => {
+        console.log(res);
+        return res.json();
+      })
+      .then((data: IChatMessage[]) => {
+        console.log(data);
+        setMessages(data);
+      })
+      .catch((err) => console.error(err));
+
+    // Cleanup function
+    return () => {
+      console.log(`Cleaning up chat room for ${roomId}`);
+      ws.disconnect();
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [roomId, username]);
+
   return (
     <div>
       <h3>Room: {roomId}</h3>
