65781d5335561eb6cec18401f5adea986dcb589b
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-10-13 15:11:18 +0700

    fix: correct import path for ChatProvider in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import { ChatProvider } from "./contexts/ChatContext.tsx";
+import { ChatProvider } from "./contexts/ChatProvider.tsx";
 import App from "./app/index.tsx";
 
 createRoot(document.getElementById("root")!).render(
