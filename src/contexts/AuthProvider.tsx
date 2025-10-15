dad80b8a0767ba49c5a90530d628e6c0867aa743
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-10-15 15:28:47 +0700

    feat: implement AuthProvider with state management for username, password, and token

--- src/contexts/AuthProvider.tsx
+++ src/contexts/AuthProvider.tsx
@@ -1,5 +1,49 @@
-import type { ReactNode } from "react";
+import { useState, type ReactNode } from "react";
+import { AuthContext } from "../hooks/useAuthContext";
+import { TOKEN_KEY, USERNAME_KEY } from "../constants/key";
 
 export function AuthProvider({ children }: { children: ReactNode }) {
-  return <div>{children}</div>;
+  const [username, setUsername] = useState<string>(
+    () => localStorage.getItem(USERNAME_KEY) || "",
+  );
+  const [password, setPassword] = useState<string>("");
+  const [token, setToken] = useState(
+    () => localStorage.getItem(TOKEN_KEY) || "",
+  );
+
+  const handleLogin = async () => {
+    try {
+      const response = await fetch("http://localhost:8080/api/auth/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ username, password }),
+      });
+
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+
+      // set username to localstorage
+      localStorage.setItem(USERNAME_KEY, username);
+
+      // set token to localstorage
+      const result = await response.json();
+      localStorage.setItem(TOKEN_KEY, result.token);
+      setToken(result.token);
+    } catch (error: unknown) {
+      console.error(error);
+    }
+  };
+
+  const value = {
+    username,
+    setUsername: (username: string) => setUsername(username),
+    password,
+    setPassword: (password: string) => setPassword(password),
+    token,
+    handleLogin,
+  };
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 }
