53f8e7d5c49d2f799fbc3c368a64c967ba5dfd64
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-10-15 15:28:19 +0700

    feat: enhance App component to handle authentication flow with token validation

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,21 +1,34 @@
+import { useAuthContext } from "../hooks/useAuthContext";
 import { useChatContext } from "../hooks/useChatContext";
+import LoginPage from "./auth/Login";
 import ChatRoom from "./chatroom";
 import Lobby from "./landing";
 import RootLayout from "./layout";
 
 function App() {
+  const { token } = useAuthContext();
   const { username, receiver, roomId } = useChatContext();
-  console.log(username, roomId);
+  console.log(token);
 
-  return (
-    <RootLayout>
-      {username == "" && roomId == "" && receiver == "" ? (
+  if (!token) {
+    return (
+      <RootLayout>
+        <LoginPage />
+      </RootLayout>
+    );
+  } else if (token && username == "" && roomId == "" && receiver == "") {
+    return (
+      <RootLayout>
         <Lobby />
-      ) : (
+      </RootLayout>
+    );
+  } else {
+    return (
+      <RootLayout>
         <ChatRoom />
-      )}
-    </RootLayout>
-  );
+      </RootLayout>
+    );
+  }
 }
 
 export default App;
