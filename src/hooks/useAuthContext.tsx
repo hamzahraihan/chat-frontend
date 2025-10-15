72d9f7435f8857de2472332c8f6b82b0651b448a
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-10-15 15:28:53 +0700

    feat: update AuthProviderProps to include setUsername, setPassword, token, and handleLogin

--- src/hooks/useAuthContext.tsx
+++ src/hooks/useAuthContext.tsx
@@ -3,11 +3,19 @@ import { createContext, useContext } from "react";
 type AuthProviderProps = {
   username: string;
   password: string;
+  setUsername: (username: string) => void;
+  setPassword: (password: string) => void;
+  token: string | null;
+  handleLogin: () => void;
 };
 
 const initialState: AuthProviderProps = {
   username: "",
   password: "",
+  setUsername: () => null,
+  setPassword: () => null,
+  token: "",
+  handleLogin: () => null,
 };
 
 export const AuthContext = createContext<AuthProviderProps>(initialState);
