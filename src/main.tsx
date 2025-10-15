0d4b808127cdd4200bfe0092a6506753f45c3b7c
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-10-15 15:28:14 +0700

    feat: nest ChatProvider inside AuthProvider for improved context management

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { ChatProvider } from "./contexts/ChatProvider.tsx";
 import App from "./app/index.tsx";
+import { AuthProvider } from "./contexts/AuthProvider.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ChatProvider>
-      <App />
-    </ChatProvider>
+    <AuthProvider>
+      <ChatProvider>
+        <App />
+      </ChatProvider>
+    </AuthProvider>
   </StrictMode>,
 );
