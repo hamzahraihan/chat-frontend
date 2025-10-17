4a5117fbeb14511a90eea1bf610ff2431092a54c
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-10-17 09:41:38 +0700

    add css style to center elements

--- src/app/auth/Login.tsx
+++ src/app/auth/Login.tsx
@@ -1,23 +1,26 @@
 import { useAuthContext } from "../../hooks/useAuthContext";
+import "./auth.css";
 
 export default function LoginPage() {
   const { handleLogin, setPassword, setUsername, password, username } =
     useAuthContext();
   return (
-    <div>
-      <input
-        type="text"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-        placeholder="username.."
-      />
-      <input
-        type="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        placeholder="password.."
-      />
-      <button onClick={handleLogin}>Login</button>
+    <div className="login-container">
+      <div className="login-card">
+        <input
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          placeholder="username.."
+        />
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="password.."
+        />
+        <button onClick={handleLogin}>Login</button>
+      </div>
     </div>
   );
 }
