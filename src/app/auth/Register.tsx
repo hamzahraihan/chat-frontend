56e6fb78870cd4a1ec2e24d6d146720986615f9a
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-10-18 15:44:17 +0700

    feat: implement RegisterPage with username and password inputs

--- src/app/auth/Register.tsx
+++ src/app/auth/Register.tsx
@@ -1,3 +1,13 @@
+import { useState } from "react";
+
 export default function RegisterPage() {
-  return <div>register page</div>;
+  return (
+    <div className="login-container">
+      <div className="login-card">
+        <input type="text" placeholder="username.." />
+        <input type="password" placeholder="password.." />
+        <button>Register</button>
+      </div>
+    </div>
+  );
 }
