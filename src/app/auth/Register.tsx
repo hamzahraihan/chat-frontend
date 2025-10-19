6d13eb618d92a2855f03c99b97a648b34b1fed31
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-10-19 14:24:41 +0700

    Update Register.tsx

--- src/app/auth/Register.tsx
+++ src/app/auth/Register.tsx
@@ -1,11 +1,23 @@
-import { useState } from "react";
+import { useAuthContext } from "../../hooks/useAuthContext";
 
 export default function RegisterPage() {
+  const { setUsername, setPassword, username, password } = useAuthContext();
+
   return (
-    <div className="login-container">
-      <div className="login-card">
-        <input type="text" placeholder="username.." />
-        <input type="password" placeholder="password.." />
+    <div className="auth-container">
+      <div className="auth-card">
+        <input
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          placeholder="username.."
+        />
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="password.."
+        />
         <button>Register</button>
       </div>
     </div>
