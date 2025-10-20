925bdbdb3225c32755da7a7c4c091bf61cf360c7
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-10-20 11:16:15 +0700

    Update index.tsx

--- src/app/auth/index.tsx
+++ src/app/auth/index.tsx
@@ -1,19 +1,20 @@
 import { useState } from "react";
 import LoginPage from "./Login";
 import RegisterPage from "./Register";
+import "./auth.css";
 
 export default function AuthPage() {
   const [page, setPage] = useState<"login" | "register">("login");
 
   return (
-    <div>
+    <div className="auth-container">
       {page == "login" ? (
-        <div>
+        <div className="auth-card">
           <LoginPage />
           <button onClick={() => setPage("register")}>Register</button>
         </div>
       ) : (
-        <div>
+        <div className="auth-card">
           <RegisterPage />
           <button onClick={() => setPage("login")}>Login</button>
         </div>
