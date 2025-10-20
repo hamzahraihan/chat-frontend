b3ebb2ca725ff03f22ea715ecb7810c35e547a73
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-10-20 11:16:39 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,20 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   define: {
-    global: "window",
+    global: 'window',
   },
   server: {
-    host: true,
+    host: '0.0.0.0',
     port: 3000,
+    proxy: {
+      '/ws': {
+        target: 'ws://localhost:8080',
+        ws: true,
+      },
+    },
   },
 });
